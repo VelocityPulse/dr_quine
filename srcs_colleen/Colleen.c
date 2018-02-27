e6f77fd2dd17cafb1c5c67b69643baeb5f85ac79
Clement CHAMEYRAT <cchameyr@e1r9p10.42.fr>
2018-02-27 16:33:09 +0100

    finish Colleen.c

---
+++ srcs_colleen/Colleen.c
@@ -0,0 +1,3 @@
+#include<stdio.h>/*comment*/
+void print(char *a){printf(a,10,34,a);}
+int main(){/*comment*/char*a="#include<stdio.h>/*comment*/%cvoid print(char *a){printf(a,10,34,a);}%1$cint main(){/*comment*/char*a=%c%s%2$c;print(a);}%1$c";print(a);}
