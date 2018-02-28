1b91db212cc2cb1faf0daa48d65eb70f1dd558fc
Clement CHAMEYRAT <cchameyr@e2r8p17.42.fr>
2018-02-28 10:27:01 +0100

    save

--- Colleen.c
+++ Colleen.c
@@ -1,3 +1,10 @@
-#include<stdio.h>/*comment*/
+#include<stdio.h>
+/*
+	comment
+*/
 void print(char *a){printf(a,10,34,a);}
-int main(){/*comment*/char*a="#include<stdio.h>/*comment*/%cvoid print(char *a){printf(a,10,34,a);}%1$cint main(){/*comment*/char*a=%c%s%2$c;print(a);}%1$c";print(a);}
+int main(){
+/*
+	comment
+*/
+char*a="#include<stdio.h>/*%1$c%4$ccomment%1$c*/%cvoid print(char *a){printf(a,10,34,a,9);}%1$cint main(){/*%1$c%4$ccomment%1$c*/char*a=%c%s%2$c;print(a);}%1$c";print(a);}
--- Grace.c
+++ Grace.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* comment */
-#define defineai
-#define defineb
-#define MAIN(X) int main(){char *a = "#include <stdio.h>%c/* comment */%1$cint main(){char*a=%c%s%2$c;FILE *f = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);printf(f, a);}%1$cvoid print(FILE *f, char *a){if (f != NULL) return ;fprintf(f, a, 10, 34, a);}%1$cMAIN(1)%1$c";FILE *f = fopen("Grace_kid.c", "w");print(f, a);}
-void print(FILE *f, char *a){if (f == NULL) return ;fprintf(f, a, 10, 34, a);}
+/*
+	comment
+*/
+#define one
+#define two
+#define MAIN(X) int main(){char *a = "#include <stdio.h>%c/*%1$c%4$ccomment%1$c*/%1$c#define one%1$c#define two%1$c#define MAIN(X) int main(){char *a = %c%s%2$c;FILE *f = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);print(f, a);}%1$cvoid print(FILE *f, char *a){if (f == NULL) return ;fprintf(f, a, 10, 34, a, 9);}%1$cMAIN(1)%1$c";FILE *f = fopen("Grace_kid.c", "w");print(f, a);}
+void print(FILE *f, char *a){if (f == NULL) return ;fprintf(f, a, 10, 34, a, 9);}
 MAIN(1)
