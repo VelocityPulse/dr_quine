bd01d5ed4cec6c380ba1d25f15eb29ea85478080
Clement CHAMEYRAT <cchameyr@e2r8p17.42.fr>
2018-02-28 13:53:48 +0100

    work in progress

--- Colleen.c
+++ Colleen.c
@@ -2,9 +2,9 @@
 /*
 	comment
 */
-void print(char *a){printf(a,10,34,a);}
+void print(char *a){printf(a,10,34,a,9);}
 int main(){
 /*
 	comment
 */
-char*a="#include<stdio.h>/*%1$c%4$ccomment%1$c*/%cvoid print(char *a){printf(a,10,34,a,9);}%1$cint main(){/*%1$c%4$ccomment%1$c*/char*a=%c%s%2$c;print(a);}%1$c";print(a);}
+char*a="#include<stdio.h>%1$c/*%1$c%4$ccomment%1$c*/%1$cvoid print(char *a){printf(a,10,34,a,9);}%1$cint main(){%1$c/*%1$c%4$ccomment%1$c*/%1$cchar*a=%c%s%2$c;print(a);}%1$c";print(a);}
