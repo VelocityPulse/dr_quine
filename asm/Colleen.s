c97175914f185885435468b48bd0ca5272b65768
Clement CHAMEYRAT <cchameyr@e2r8p17.42.fr>
2018-03-02 13:13:32 +0100

    save

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -1,7 +1,22 @@
+%define MACH_SYSCALL(nb) 0x2000000 | nb
+
 section .data
+	msg: db "hello world!", 10
+	len: equ $-msg
 
 section .text
 global start
+global _main
 
 start:
+call _main
+ret
+
+_main:
+mov rax, 4
+mov rbx, 1
+mov rcx, msg
+mov rdx, len
+mov rax, MACH_SYSCALL(4)
+syscall
 ret
