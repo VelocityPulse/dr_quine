32f7cb5587754a829586d52d70a39edb57187c32
Clement CHAMEYRAT <cchameyr@e3r9p15.42.fr>
2018-03-05 13:53:04 +0100

    add different comments on colleen.c

--- Colleen.c
+++ Colleen.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /*
-	comment
+	comment1
 */
 void print(char *a){printf(a,10,34,a,9);}
 int main(){
 /*
-	comment
+	comment2
 */
-char*a="#include<stdio.h>%1$c/*%1$c%4$ccomment%1$c*/%1$cvoid print(char *a){printf(a,10,34,a,9);}%1$cint main(){%1$c/*%1$c%4$ccomment%1$c*/%1$cchar*a=%c%s%2$c;print(a);}%1$c";print(a);}
+char*a="#include<stdio.h>%1$c/*%1$c%4$ccomment1%1$c*/%1$cvoid print(char *a){printf(a,10,34,a,9);}%1$cint main(){%1$c/*%1$c%4$ccomment2%1$c*/%1$cchar*a=%c%s%2$c;print(a);}%1$c";print(a);}
