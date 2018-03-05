ab80d9f22963584fbce83c145fe2802baba78da0
Clement CHAMEYRAT <cchameyr@e3r9p15.42.fr>
2018-03-05 13:57:16 +0100

    work on colleen asm

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -1,20 +1,26 @@
-%define MACH_SYSCALL(nb) 0x2000000 | nb
-%define STDOUT 1
-%define WRITE 4
-
+/*
+	comment1
+*/
 section .data
-hello:
-	.msg db "hello world!", 10, 0
-	.len equ $-hello.msg
-
+nl db 10, 0
+qmark db 34, 0
+tab db 9, 0
+fmt db "section .data%snl db 10, 0%1$sqmark db 34, 0%1$sfmt db %2$s%3$s%2$s, 0%1$ssection .text%1$sglobal _main%1$sextern _printf%1$s_main:%1$spush rbp%1$smov rbp, rsp%1$slea rdi, [rel fmt]%1$slea rsi, [rel nl]%1$slea rdx, [rel qmark]%1$slea rcx, [rel fmt]%1$scall _printf%1$sleave%1$sret%1$s", 0
 section .text
 global _main
 extern _printf
-
 _main:
+/*
+	comment2
+*/
+
+; todo demander ce qu'il y a apres rcx
 push rbp
 mov rbp, rsp
-lea rdi, [rel hello.msg]
+lea rdi, [rel fmt]
+lea rsi, [rel nl]
+lea rdx, [rel qmark]
+lea rcx, [rel fmt]
 call _printf
 leave
 ret
