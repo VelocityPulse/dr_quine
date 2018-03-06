2804670fbc0746ce0bd13d34a79a2e02fc50cf5f
Clement CHAMEYRAT <cchameyr@e2r8p18.42.fr>
2018-03-06 12:06:11 +0100

    update grace asm

--- asm/Grace.s
+++ asm/Grace.s
@@ -4,21 +4,43 @@
 %endmacro
 
 %macro main 0
+section .data
+nl db 10, 0
+qmark db 34, 0
+comment db 59, 0
+kidname db "Grace_kid.s", 0
+fmt db "test test test", 10, 0
+
+debug db "debug : %d", 10, 0
+
+
+section .text
+global _main
+extern _dprintf
+extern _printf ; to remove
+_main:
 push rbp
 mov rbp, rsp
+
+mov rax, 0x2000005
+lea rdi, [rel kidname]
+mov rsi, 0x201
+mov rdx, 666o
+syscall
+
+mov r15, rax
+mov rdi, r15
+lea rsi, [rel fmt]
+lea rdx, [rel nl]
+lea rcx, [rel qmark]
+lea r8, [rel fmt]
+lea r9, [rel comment]
+call _dprintf
+
+mov rax, 0x2000006
+mov rdi, r15
+syscall
 leave
 ret
 %endmacro
-
-
-section .data
-;nl db 10, 0
-;qmark db 34, 0
-;comment db 59, 0
-;fmt db ""
-section .text
-global start
-global main
-extern _printf
-start:
-	call main
+main
