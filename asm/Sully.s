9fcf168fa312a980cc2c379845ab4515e5c73732
Clement CHAMEYRAT <cchameyr@e3r9p15.42.fr>
2018-03-07 12:37:18 +0100

    FUNCTION in sully

--- asm/Sully.s
+++ asm/Sully.s
@@ -4,28 +4,36 @@ qmark db 34, 0
 fmt_compile db "", 0
 fmt_copy db "", 0
 fmt_sully_x db "Sully_%d.s", 0
-fmt_debug db "debug %d", 10, 0 ; to remove
+fmt_debug db "debug %d", 10, 0 ; TO REMOVE
 
 section .text
 global _main
+global _debug ; TO REMOVE
 extern _dprintf
 extern _sprintf
-extern _printf ; to remove
+extern _printf ; TO REMOVE
+
+_debug: ; TO REMOVE
+push rbp ; TO REMOVE
+mov rbp, rsp ; TO REMOVE
+lea rdi, [rel fmt_debug] ; TO REMOVE
+mov rsi, 1 ; TO REMOVE
+call _printf ; TO REMOVE
+leave ; TO REMOVE
+ret ; TO REMOVE
 
 _main:
 push rbp
 mov rbp, rsp
 
-push 0
+push 2
 pop rsi
 cmp rsi, 0
 jle exit
 
-lea rdi, [rel fmt_debug]
-mov rsi, 1
-call _printf
-
+call _debug ; TO REMOVE
 
 exit:
 leave
 ret
+
